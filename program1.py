3a6db7b8be42264c4252fe6b6f9da586f92b84c1
Hannafaith B. Punzal <92629367+hannapunzal@users.noreply.github.com>
2021-10-27 00:38:58 +0800

    This program prints the total amount of apples and oranges the user wants to buy

--- program1.py
+++
@@ -1,4 +0,0 @@
-age = input("Enter your age: ")
-name = input("Enter your name: ")
-live = input("Enter your address: ")
-print("Hi, my name is " + name + "." " I am " + age + " years old and I live in " + live + "." )
