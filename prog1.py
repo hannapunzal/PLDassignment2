4d892b9dbfee58c1ba39dca12db50495bc66485e
Hannafaith B. Punzal <92629367+hannapunzal@users.noreply.github.com>
2021-11-20 18:36:10 +0800

    This program asks and displays the user's name, age and address.

---
+++ prog1.py
@@ -0,0 +1,4 @@
+age = int(input("Enter your age: "))
+name = input("Enter your name: ")
+live = input("Enter your address: ")
+print(f"Hi, my name is {name}. I am {age} years old and I live in {live}.")
