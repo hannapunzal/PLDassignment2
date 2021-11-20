f243411ce83525c84f1e9d2529eff3501f59ff69
Hannafaith B. Punzal <92629367+hannapunzal@users.noreply.github.com>
2021-11-20 18:36:55 +0800

    This program displays the total amount for apples and oranges that the user wants to purchase.

---
+++ prog2.py
@@ -0,0 +1,10 @@
+apple_Question = int(input("How many apples do you want to buy? "))
+orange_Question = int(input("How many oranges do you want to buy? "))
+
+apple_Price = 20
+orange_Price = 35
+
+apple_Total = apple_Question * apple_Price
+orange_Total = orange_Question * orange_Price
+total = float(apple_Total + orange_Total)
+print(f"The total amount for the apples and oranges is PHP{total: .2f}.")
